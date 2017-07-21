1588062c503a4a003d88a9c97e9511e58796eefc
Anthony COLAS <acolas@e1r3p18.42.fr>
2017-07-21 16:59:16 +0200

    test

--- get_next_line.c
+++ get_next_line.c
@@ -6,87 +6,51 @@
 /*   By: acolas <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/09 19:31:16 by acolas            #+#    #+#             */
-/*   Updated: 2017/07/16 20:59:56 by acolas           ###   ########.fr       */
+/*   Updated: 2017/07/21 16:58:26 by acolas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-int		ft_new_line(char *str, char *stat)
+int		ft_new_line(char *stock, char *stat)
 {
-	int				i;
 	char			*tmp;
+	int i;
 
+	stat = ft_strdup(ft_strchr(stock, '\n'));
 	i = 0;
-	printf("5\n");
-	tmp = ft_strdup(ft_strchr(str + 1, '\n'));
-	ft_memdel((void **)stat);
-	ft_strcpy(stat, str);
-	printf("OK");
-	return(0);
+	while (stock[i] != '\n')
+		++i;
+	stock[i] = '\0';	
+//	ft_memdel((void **)stock);
+//	ft_memdel((void **)tmp);
+	return(END);
 }
 
 int		get_next_line(const int fd, char **line)
 {
 
-	// Les valeurs de retour !
-	// 1: au cas ou on renvoie une chaine!
-	// 0: si on renvoit rien !
-	// -1: fail!!!
-	// read renvoit -1 tu renvoie -1
-	// si buff_size est negatif tu renvoit -1
-	//
-	// char **line -> c un char *line de main
-	// et on recup l'adresse car on va alloue une chaine pour cette **line!
-	//
-	// l'objectif:
-	// recup une ligne entre les \n \n ou sinon \0
-	//
-	// strjoin = si buffer lu mais pas de \n
-	// strchr = si \n dans buffer
-	//
-	// un static char a utilise !!!!
-	//
-	// text.txt:
-	// abcdef
-	// 012345
-	// 678901
-	//
 	static char		*stat = NULL;
 	char			buff[BUFF_SIZE + 1];		
-	int				i;
-	char			*str;
-	char			*tmp;
+	int				ret;
+	char			*stock;
 
-	i = 0;
-	tmp = ft_strnew(BUFF_SIZE + 1);
-	if (!(line = (char **)malloc(sizeof(char *) * BUFF_SIZE + 1)))
-			return (0);
-	if (stat == 0)
-		str = ft_strnew(BUFF_SIZE + 1);
+	if (stat)
+		stock = ft_strdup(stat);
 	else
-		str = stat;
-	printf("2\n");
-	while (ft_strchr(tmp, '\n') == NULL)
+		stock = ft_strnew(0);
+	while (ft_strchr(stock, '\n') == NULL)
 	{	
-		if (read(fd, buff, BUFF_SIZE) < 0 || BUFF_SIZE < 0 || fd < 0)
-		{
-			printf("fail 1");
-			return (-1);
-		}
-		tmp = ft_strjoin(str, buff);
+		if ((ret = read(fd, buff, BUFF_SIZE)) <= 0 || BUFF_SIZE < 0 || fd < 0)
+			return (ERROR);
+		buff[ret] = '\0';
+		stock = ft_strjoin(stock, buff);
 	}
-	printf("3\n");
-	while (tmp[i] != '\n')
-		i++;
-	ft_strncpy(*line, tmp, i);
-	if (ft_new_line(str, stat) != 0)
-	{
-		printf("fail 3");
-		return (-1);
-	}
-	return (0);
+	if (ft_new_line(stock, stat) != 0)
+		return (ERROR);
+	*line = ft_strdup(stock);
+	return (END);
 }
 
 int		main(void)
@@ -94,7 +58,10 @@ int		main(void)
 	int fd = open("test1", O_RDONLY);
 	char	*line = NULL;
 
-	printf("1\n");
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
 	get_next_line(fd, &line);
 	printf("%s\n", line);
 	close(fd);
--- get_next_line_v2.c
+++ get_next_line_v2.c
@@ -1,25 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_v2.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acolas <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/06/09 19:33:29 by acolas            #+#    #+#             */
-/*   Updated: 2017/07/21 15:18:31 by acolas           ###   ########.fr       */
+/*   Created: 2017/07/21 15:37:48 by acolas            #+#    #+#             */
+/*   Updated: 2017/07/21 15:51:27 by acolas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-# define BUFF_SIZE 10
-# define ERROR -1
-# define OK 1
-# define END 0
-# include "libft/libft.h"
-# include <unistd.h>
-# include <fcntl.h>
+#include "get_next_line.h"
+#include <stdio.h>
 
-int		get_next_line(const int fd, char **line);
+int		get_next_line(const int fd, char **line)
+{
+	static char		*stat = NULL;
+	char 			buff[BUFF_SIZE + 1];
+	char			*stock;
 
-#endif
+	if (!stock)
+		stock = ft_strnew(0);
+	if (ft_strchr(stock, '\n') != 0)
+		ft_getline(stock);
+
+	
+}
