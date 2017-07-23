573c8c6194170b39394e8dcf13c51fe2c9cde4bf
Anthony COLAS <acolas@e2r1p14.42.fr>
2017-07-23 18:49:46 +0200

    presque fini

--- get_next_line.c
+++ get_next_line.c
@@ -6,42 +6,59 @@
 /*   By: acolas <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/22 21:56:46 by acolas            #+#    #+#             */
-/*   Updated: 2017/07/22 22:30:27 by acolas           ###   ########.fr       */
+/*   Updated: 2017/07/23 18:48:13 by acolas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 int		ft_new_line(char *stock, char **stat)
 {
 	int		i;
+	int		len;
 
+	ft_memdel((void **)stat);
 	*stat = ft_strdup(ft_strchr(stock, '\n') + 1);
 	i = 0;
-	while (stock[i] != '\n')
+	if (!stock)
+		return (END);
+	len = ft_strlen(stock);	
+	while (stock[i] != '\n' && len > 0)
+	{
 		++i;
+		len--;
+	}
 	stock[i] = '\0';
-	// ft_memdel((void **)stock);
 	return (END);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-
 	static char		*stat = NULL;
 	char			buff[BUFF_SIZE + 1];
 	int				ret;
 	char			*stock;
 
 	if (stat)
+	{	
 		stock = ft_strdup(stat);
+		if (ft_strchr(stock, '\n') !=  0)
+			stat = ft_strdup(ft_strchr(stock, '\n') + 1);
+	}
 	else
 		stock = ft_strnew(0);
 	while (ft_strchr(stock, '\n') == NULL)
 	{
+		ret = 0;
 		if ((ret = read(fd, buff, BUFF_SIZE)) < 0 || BUFF_SIZE < 0 || fd < 0)
 			return (ERROR);
 		buff[ret] = '\0';
+		if (ret == 0)
+		{
+			ft_memdel((void **)line);
+			return (END);
+		}
 		stock = ft_strjoin(stock, buff);
 	}
 	if (ft_new_line(stock, &stat) != 0)
@@ -49,3 +66,15 @@ int		get_next_line(const int fd, char **line)
 	*line = ft_strdup(stock);
 	return (OK);
 }
+/*
+int		main(void)
+{
+	int fd = open("test3", O_RDONLY);
+	char	*line = NULL;
+	int		ret;
+
+	while ((ret = get_next_line(fd, &line)))
+		printf("line = %s\nval ret = %d\n", line, ret);
+	close(fd);
+}
+*/
--- main.c
+++ main.c
@@ -1,6 +1,32 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acolas <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/23 12:21:33 by acolas            #+#    #+#             */
+/*   Updated: 2017/07/23 14:49:55 by acolas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	main(void)
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "get_next_line.h"
+
+int	get_next_line(const int fd, char **line);
+
+int		main(void)
 {
-	write(1, "abcdefghijklmnopqrstuvwxz\n", 27);
+	int fd = open("test1", O_RDONLY);
+	char	*line = NULL;
+
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	get_next_line(fd, &line);
+	printf("%s\n", line);
+	close(fd);
 }
